4266f36e06de61ff5e09c422a10002d9d625f552
Josh <116621485+Santuikows-DevFX@users.noreply.github.com>
2023-12-27 19:58:24 +0800

    Files

---
+++ RealTimeAddress/RealTimeAddress/Address.Designer.cs
@@ -0,0 +1,156 @@
+﻿namespace RealTimeAddress
+{
+    partial class Address
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.cmbRegion = new System.Windows.Forms.ComboBox();
+            this.cmbProvince = new System.Windows.Forms.ComboBox();
+            this.cmbCity = new System.Windows.Forms.ComboBox();
+            this.cmbBarangay = new System.Windows.Forms.ComboBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // cmbRegion
+            // 
+            this.cmbRegion.FormattingEnabled = true;
+            this.cmbRegion.Location = new System.Drawing.Point(54, 131);
+            this.cmbRegion.Name = "cmbRegion";
+            this.cmbRegion.Size = new System.Drawing.Size(494, 28);
+            this.cmbRegion.TabIndex = 0;
+            // 
+            // cmbProvince
+            // 
+            this.cmbProvince.FormattingEnabled = true;
+            this.cmbProvince.Location = new System.Drawing.Point(54, 192);
+            this.cmbProvince.Name = "cmbProvince";
+            this.cmbProvince.Size = new System.Drawing.Size(494, 28);
+            this.cmbProvince.TabIndex = 1;
+            // 
+            // cmbCity
+            // 
+            this.cmbCity.FormattingEnabled = true;
+            this.cmbCity.Location = new System.Drawing.Point(54, 256);
+            this.cmbCity.Name = "cmbCity";
+            this.cmbCity.Size = new System.Drawing.Size(494, 28);
+            this.cmbCity.TabIndex = 2;
+            this.cmbCity.SelectedIndexChanged += new System.EventHandler(this.cmbCity_SelectedIndexChanged);
+            // 
+            // cmbBarangay
+            // 
+            this.cmbBarangay.FormattingEnabled = true;
+            this.cmbBarangay.Location = new System.Drawing.Point(54, 319);
+            this.cmbBarangay.Name = "cmbBarangay";
+            this.cmbBarangay.Size = new System.Drawing.Size(494, 28);
+            this.cmbBarangay.TabIndex = 3;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(50, 108);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(60, 20);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "Region";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(50, 169);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(69, 20);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "Province";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(50, 233);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(35, 20);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "City";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(50, 296);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(77, 20);
+            this.label4.TabIndex = 7;
+            this.label4.Text = "Barangay";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 26F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(145, 34);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(316, 59);
+            this.label5.TabIndex = 8;
+            this.label5.Text = "ADDRESS: ";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(626, 430);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.cmbBarangay);
+            this.Controls.Add(this.cmbCity);
+            this.Controls.Add(this.cmbProvince);
+            this.Controls.Add(this.cmbRegion);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ComboBox cmbRegion;
+        private System.Windows.Forms.ComboBox cmbProvince;
+        private System.Windows.Forms.ComboBox cmbCity;
+        private System.Windows.Forms.ComboBox cmbBarangay;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+    }
+}
+
---
+++ RealTimeAddress/RealTimeAddress/Address.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace RealTimeAddress
+{
+    public partial class Address : Form
+    {
+        public Address()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            cmbRegion.Items.Add("Metro Manila");
+            cmbProvince.Items.Add("Metro Manila");
+
+            string[] cities = { "Valenzuela", "Ermita" };
+            foreach(string city in cities)
+            {
+                cmbCity.Items.Add(city);
+            }
+
+           
+
+        }
+
+        private void cmbCity_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            ArrayList barangays = new ArrayList();
+            if(cmbCity.Text.Equals("Valenzuela"))
+            {
+                cmbBarangay.Items.Clear();
+                cmbBarangay.Text = "";
+                barangays.Add("Barangay In Val #1");
+                barangays.Add("Barangay In Val #2");
+                barangays.Add("Barangay In Val #3");
+                barangays.Add("Barangay In Val #4");
+
+                for (int i = 0; i < barangays.Count; i++)
+                {
+                    cmbBarangay.Items.Add(barangays[i]);
+                }
+            }else
+            {
+                cmbBarangay.Items.Clear();
+                cmbBarangay.Text = "";
+                barangays.Add("Barangay In Ermita #1");
+                barangays.Add("Barangay In Ermita #2");
+                barangays.Add("Barangay In Ermita #3");
+                barangays.Add("Barangay In Ermita #4");
+
+                for (int i = 0; i < barangays.Count; i++)
+                {
+                    cmbBarangay.Items.Add(barangays[i]);
+                }
+
+            }
+        }
+    }
+}
